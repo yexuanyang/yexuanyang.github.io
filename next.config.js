a0008c82ec218ce6b3f0ad5a6843771b02aa164d
Yexuan Yang <myemailyyxg@gmail.com>
2025-02-20 21:00:42 +0800

    v0.3.4

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//,"");
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
+const repo = 'yexuanyang.github.io';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
 
 const nextConfig = {
     basePath,
