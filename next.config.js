62d9c20da332703291f670d0400117a36bd28293
Yexuan Yang <myemailyyxg@gmail.com>
2025-02-20 20:04:46 +0800

    v0.3.1

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
-const repo = "yexuanyang.github.io";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+if (isGithubActions) {
+    // 去掉 `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+}
 const nextConfig = {
-    reactStrictMode: true,
-    output: "export",
     basePath,
     assetPrefix,
+    output: "export",
     images: {
         unoptimized: true,
     },
