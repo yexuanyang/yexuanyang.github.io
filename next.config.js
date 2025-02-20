a05d23da95c90a8d245541fa81cc52331bacd7c4
Yexuan Yang <myemailyyxg@gmail.com>
2025-02-20 21:06:50 +0800

    v0.4.1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const repo = '';
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
 
 const nextConfig = {
-    basePath,
-    assetPrefix,
     output: "export",
     images: {
         unoptimized: true,
